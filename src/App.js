63555f2bc4a424c2bd974c9778ffc0256e168fd9
Sruniraj <pcsruniraj@gmail.com>
2018-07-10 10:48:09 +0000

    App.js file edited with versin change

--- src/App.js
+++ src/App.js
@@ -8,11 +8,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-<<<<<<< HEAD
           <h1 className="App-title">Welcome to V1 Version</h1>
-=======
-          <h1 className="App-title">Welcome to My Website</h1>
->>>>>>> 318dd3db5090ab554ce810eaa24c30c06a6b1da6
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
